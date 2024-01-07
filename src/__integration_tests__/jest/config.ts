c8d2a35adde5ba2d60c8df78bff9f52fdb8eed96
santichai.sak <santichai.saksoong@gmail.com>
2024-01-07 17:33:04 +0700

    add Unit test & Integration test

---
+++ src/__integration_tests__/jest/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  Memory: true,
+  MongoURI:
+    "mongodb+srv://santichaisaksoong:d7opyAus1dNjQCoj@next-innovation-db.xfe1lr0.mongodb.net",
+  Database: "test",
+};
+
+export default config;
